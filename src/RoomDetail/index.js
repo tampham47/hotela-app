51e36778aff4fc4cc26ef41b759acbc1c40062f9
Tam Pham <tampham47@live.com>
2017-06-14 19:42:12 +0700

    add detail page

--- src/RoomDetail/index.js
+++ src/RoomDetail/index.js
@@ -5,6 +5,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Navbar from '../components/Navbar';
+import request from 'request';
 
 import './styles.css';
 
@@ -17,13 +18,29 @@ class App extends Component {
 
     this.state = {
       form: {},
+      roomDetail: null,
     };
 
     this.onNameChange = this.onNameChange.bind(this);
     this.onPostComment = this.onPostComment.bind(this);
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    const id = this.props.match.params.id;
+
+    request({
+      method: 'get',
+      url: 'http://35.162.241.129:3000/api/v1/room/' + id,
+    }, function (error, response, body) {
+      const data = JSON.parse(body);
+      console.log('Roomdetail error:', error);
+      console.log('Roomdetail data:', typeof data, data);
+
+      this.setState({
+        roomDetail: data
+      })
+    }.bind(this));
+  }
 
   onPostComment(e) {
     e.preventDefault();
@@ -40,192 +57,73 @@ class App extends Component {
   }
 
   render() {
+    const room = this.state.roomDetail;
+
     return (
       <div>
         <Navbar title="Room Detail" />
 
-        <div className="panel-control-left">
-          <ul id="slide-out-left" className="side-nav collapsible"  data-collapsible="accordion">
-            <li>
-              <a href="index.html"><i className="fa fa-home"></i>Home</a>
-            </li>
-            <li>
-              <div className="collapsible-header"><i className="fa fa-file-text-o"></i>Categories<span><i className="fa fa-chevron-right"></i></span></div>
-              <div className="collapsible-body">
-                <ul className="categories-in collapsible"  data-collapsible="accordion">
-                  <li><a href="categories.html">Luxury Room</a></li>
-                  <li><a href="categories.html">Beautiful Room</a></li>
-                  <li><a href="categories.html">Suite Room</a></li>
-                  <li><a href="categories.html">Family Room</a></li>
-                  <li><a href="categories.html">Double Room</a></li>
-                </ul>
-              </div>
-            </li>
-            <li>
-              <a href="room.html"><i className="fa fa-th-large"></i>Room List</a>
-            </li>
-            <li>
-              <a href="details.html"><i className="fa fa-list"></i>Room Details</a>
-            </li>
-            <li>
-              <a href="shopping-cart.html"><i className="fa fa-shopping-cart"></i>Shopping Cart</a>
-            </li>
-            <li>
-              <a href="book-now.html"><i className="fa fa-send"></i>Book Now</a>
-            </li>
-            <li>
-              <a href="gallery.html"><i className="fa fa-image"></i>Gallery</a>
-            </li>
-            <li>
-              <div className="collapsible-header"><i className="fa fa-rss"></i>Blog <span><i className="fa fa-chevron-right"></i></span></div>
-              <div className="collapsible-body">
-                <ul className="side-nav-panel">
-                  <li><a href="blog.html">Blog</a></li>
-                  <li><a href="blog-single.html">Blog Single</a></li>
-                </ul>
-              </div>
-            </li>
-            <li>
-              <div className="collapsible-header"><i className="fa fa-file-powerpoint-o"></i>Pages <span><i className="fa fa-chevron-right"></i></span></div>
-              <div className="collapsible-body">
-                <ul className="side-nav-panel">
-                  <li><a href="faq.html">Faq</a></li>
-                  <li><a href="testimonial.html">Testimonial</a></li>
-                  <li><a href="404.html">404 Page</a></li>
-                </ul>
-              </div>
-            </li>
-            <li>
-              <a href="contact.html"><i className="fa fa-envelope"></i>Contact Us</a>
-            </li>
-            <li>
-              <a href="login.html"><i className="fa fa-sign-in"></i>Login</a>
-            </li>
-            <li>
-              <a href="register.html"><i className="fa fa-user-plus"></i>Register</a>
-            </li>
-          </ul>
-        </div>
+        {!room && (
+          <span>Loading...</span>
+        )}
 
-        <div className="panel-control-right">
-          <div id="slide-out-right" className="side-nav">
-            <div className="row entry">
-              <div className="col s4">
-                <img src="img/cart1.jpg" alt="" />
+        {room && (
+          <div className="details app-pages app-section">
+            <div className="container">
+              <img src={"http://35.162.241.129:3000/uploads/" + room.coverImage.filename} alt="" />
+              <h3>{room.title}</h3>
+              <div className="rating">
+                <span className="active"><i className="fa fa-star"></i></span>
+                <span className="active"><i className="fa fa-star"></i></span>
+                <span className="active"><i className="fa fa-star"></i></span>
+                <span className="active"><i className="fa fa-star"></i></span>
+                <span className=""><i className="fa fa-star"></i></span>
               </div>
-              <div className="col s6">
-                <div className="desc">
-                  <h6>Luxury Room</h6>
-                  <div className="rating">
-                    <span className="active"><i className="fa fa-star"></i></span>
-                    <span className="active"><i className="fa fa-star"></i></span>
-                    <span className="active"><i className="fa fa-star"></i></span>
-                    <span className="active"><i className="fa fa-star"></i></span>
-                    <span className=""><i className="fa fa-star"></i></span>
-                  </div>
-                  <h6><span>$150.00</span></h6>
-                </div>
-              </div>
-              <div className="col s2">
-                <div className="action">
-                  <i className="fa fa-remove"></i>
-                </div>
-              </div>
-            </div>
-            <div className="row entry">
-              <div className="col s4">
-                <img src="img/cart2.jpg" alt="" />
-              </div>
-              <div className="col s6">
-                <div className="desc">
-                  <h6>Beautiful Room</h6>
-                  <div className="rating">
-                    <span className="active"><i className="fa fa-star"></i></span>
-                    <span className="active"><i className="fa fa-star"></i></span>
-                    <span className="active"><i className="fa fa-star"></i></span>
-                    <span className=""><i className="fa fa-star"></i></span>
-                    <span className=""><i className="fa fa-star"></i></span>
-                  </div>
-                  <h6><span>$150.00</span></h6>
-                </div>
-              </div>
-              <div className="col s2">
-                <div className="action">
-                  <i className="fa fa-remove"></i>
-                </div>
-              </div>
-            </div>
-            <div className="row price">
-              <div className="col s8">
-                <h6>Total</h6>
-              </div>
-              <div className="col s4">
-                <h6>$300.00</h6>
-              </div>
-            </div>
-            <ul>
-              <li>
-                <button className="button">Book Now</button>
-              </li>
-              <li>
-                <button className="button">View Cart</button>
-              </li>
-            </ul>
-          </div>
-        </div>
 
-        <div className="details app-pages app-section">
-          <div className="container">
-            <img src="img/details.jpg" alt="" />
-            <h3>Beautiful & Luxury Room</h3>
-            <div className="rating">
-              <span className="active"><i className="fa fa-star"></i></span>
-              <span className="active"><i className="fa fa-star"></i></span>
-              <span className="active"><i className="fa fa-star"></i></span>
-              <span className="active"><i className="fa fa-star"></i></span>
-              <span className=""><i className="fa fa-star"></i></span>
-            </div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium ducimus ipsam, suscipit, nobis necessitatibus eum odio est laborum fugiat veniam explicabo qui laboriosam! Repellat aspernatur doloribus, nulla, hic odio natus!</p>
-            <ul className="button-ul">
-              <li><button className="button">Book Now</button></li>
-              <li><button className="button">Add to Card</button></li>
-            </ul>
-            <div className="comment">
-              <h6>2 Comment</h6>
-              <div className="content">
-                <img src="img/comment1.png" alt="" />
-                <div className="entry">
-                  <strong><a href="">John Doe</a></strong>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing</p>
+              <div dangerouslySetInnerHTML={{__html: room.content}}></div>
+
+              <ul className="button-ul">
+                <li><button className="button">Book Now</button></li>
+                <li><button className="button">Add to Card</button></li>
+              </ul>
+              <div className="comment">
+                <h6>2 Comment</h6>
+                <div className="content">
+                  <img src="img/comment1.png" alt="" />
+                  <div className="entry">
+                    <strong><a href="">John Doe</a></strong>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing</p>
+                  </div>
+                </div>
+                <div className="content">
+                  <img src="img/comment2.png" alt="" />
+                  <div className="entry">
+                    <strong><a href="">John Doe</a></strong>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing</p>
+                  </div>
                 </div>
               </div>
-              <div className="content">
-                <img src="img/comment2.png" alt="" />
-                <div className="entry">
-                  <strong><a href="">John Doe</a></strong>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing</p>
+              <div className="post-comment">
+                <h6>Post Comments</h6>
+                <div className="content">
+                  <form action="#">
+                    <input type="text" placeholder="Name 2"
+                      value={this.state.form.name}
+                      onChange={this.onNameChange} />
+                    <input type="email" placeholder="Email"
+                      value={this.state.form.email} />
+                    <input type="text" placeholder="Website"
+                      value={this.state.form.website} />
+                    <textarea cols="20" rows="10" placeholder="Comment"
+                      value={this.state.form.comment}></textarea>
+                    <button className="button" onClick={this.onPostComment}>Post Comment</button>
+                  </form>
                 </div>
-              </div>
-            </div>
-            <div className="post-comment">
-              <h6>Post Comments</h6>
-              <div className="content">
-                <form action="#">
-                  <input type="text" placeholder="Name 2"
-                    value={this.state.form.name}
-                    onChange={this.onNameChange} />
-                  <input type="email" placeholder="Email"
-                    value={this.state.form.email} />
-                  <input type="text" placeholder="Website"
-                    value={this.state.form.website} />
-                  <textarea cols="20" rows="10" placeholder="Comment"
-                    value={this.state.form.comment}></textarea>
-                  <button className="button" onClick={this.onPostComment}>Post Comment</button>
-                </form>
               </div>
             </div>
           </div>
-        </div>
+        )}
+
         <footer>
           <div className="container">
             <h6>Find & follow us</h6>
